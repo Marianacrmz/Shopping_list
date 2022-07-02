1e7af563424572bffc5f4c9ef476751170241f33
Marianacrmz <marianacramirez.18@gmail.com>
2022-07-01 22:27:50 -0500

    Shopping-List

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Shopping-List-JuanPablo'`, () => {
+  it(`should have as title 'Shopping-List-Mariana'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Shopping-List-JuanPablo');
+    expect(app.title).toEqual('Shopping-List-Mariana');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Shopping-List-JuanPablo app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Shopping-List-Mariana app is running!');
   });
 });
